7567d3f4256e03bfccec67ce56f7a6bbf0c4c6a8
Pietro Cannalire <pierocann93@hotmail.it>
2018-09-28 10:26:59 +0200

    Improve backup and restore with parameters.

--- backup-freeipa-files.sh
+++ backup-freeipa-files.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
-rootDir=/tmp
+#default: /etc/security/keytabs/freeipa-backup
 
-mkdir -p /tmp/etc/sssd
-mkdir -p /tmp/etc/ipa
-mkdir -p /tmp/etc/openldap
-mkdir -p /tmp/etc/pki/nssdb
-mkdir -p /tmp/etc/sysconfig
-mkdir -p /tmp/etc/sysconfig/network
-mkdir -p /tmp/etc/ntp
+if [ -n $1 ] ; then
+	destRootDir=$1
+else
+	destRootDir=/etc/security/keytabs/freeipa-backup
+fi
 
-cp -f /etc/sssd/sssd.conf		$rootDir/etc/sssd/sssd.conf
-#cp -f /etc/ntp.conf            $rootDir/etc/ntp.conf
-cp -f /etc/sysconfig/ntpd       $rootDir/etc/sysconfig/
-cp -f /etc/ntp/step-tickers     $rootDir/etc/ntp/step-tickers
-cp -f /etc/krb5.conf            $rootDir/etc/krb5.conf
-cp -f /etc/ipa/ca.crt           $rootDir/etc/ipa/ca.crt
-cp -f /etc/ipa/default.conf     $rootDir/etc/ipa/default.conf
-cp -f /etc/openldap/ldap.conf   $rootDir/etc/openldap/ldap.conf
-cp -f /etc/pki/nssdb/*          $rootDir/etc/pki/nssdb
-cp -f /etc/krb5.keytab          $rootDir/etc/krb5.keytab
+if [ ! -d $destRootDir ] ; then
+	mkdir -p $destRootDir
+fi
 
-cp -f /etc/hosts                $rootDir/etc/hosts
-cp -f /etc/hostname             $rootDir/etc/hostname
-cp -f /etc/hostname             $rootDir/etc/hostname.ipa-client
+mkdir -p $destRootDir/etc/sssd
+mkdir -p $destRootDir/etc/ipa
+mkdir -p $destRootDir/etc/openldap
+mkdir -p $destRootDir/etc/pki/nssdb
+mkdir -p $destRootDir/etc/sysconfig
+mkdir -p $destRootDir/etc/sysconfig/network
+mkdir -p $destRootDir/etc/ntp
+
+cp -f /etc/sssd/sssd.conf		$destRootDir/etc/sssd/sssd.conf
+#cp -f /etc/ntp.conf            $destRootDir/etc/ntp.conf
+cp -f /etc/sysconfig/ntpd       $destRootDir/etc/sysconfig/
+cp -f /etc/ntp/step-tickers     $destRootDir/etc/ntp/step-tickers
+cp -f /etc/krb5.conf            $destRootDir/etc/krb5.conf
+cp -f /etc/ipa/ca.crt           $destRootDir/etc/ipa/ca.crt
+cp -f /etc/ipa/default.conf     $destRootDir/etc/ipa/default.conf
+cp -f /etc/openldap/ldap.conf   $destRootDir/etc/openldap/ldap.conf
+cp -f /etc/pki/nssdb/*          $destRootDir/etc/pki/nssdb
+cp -f /etc/krb5.keytab          $destRootDir/etc/krb5.keytab
+
+cp -f /etc/hosts                $destRootDir/etc/hosts
+cp -f /etc/hostname             $destRootDir/etc/hostname
+cp -f /etc/hostname             $destRootDir/etc/hostname.ipa-client
 
 
 
--- restore-freeipa-files.sh
+++ restore-freeipa-files.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-rootDir=/tmp
+if [ -n $1 ] ; then
+	if [ -d $1 ] ; then
+		sourceRootDir=$1
+	else
+		echo "Directory $1 does not exists"
+		exit 1
+	fi
+else
+	sourceRootDir=/etc/security/keytabs/freeipa-backup
+fi
 
-if [ -d "$rootDir/etc" ]; then
 
-	/bin/mv -f $rootDir/etc/sssd/sssd.conf 		/etc/sssd/sssd.conf
-	/bin/mv -f $rootDir/etc/sysconfig/ntpd		/etc/sysconfig/
-	/bin/mv -f $rootDir/etc/ntp/step-tickers	/etc/ntp/step-tickers
-	/bin/mv -f $rootDir/etc/krb5.conf			/etc/krb5.conf
-	/bin/mv -f $rootDir/etc/ipa/ca.crt 			/etc/ipa/ca.crt
-	/bin/mv -f $rootDir/etc/ipa/default.conf 	/etc/ipa/default.conf
-	/bin/mv -f $rootDir/etc/openldap/ldap.conf 	/etc/openldap/ldap.conf
-	/bin/mv -f $rootDir/etc/pki/nssdb/* 		/etc/pki/nssdb
-	/bin/mv -f $rootDir/etc/krb5.keytab 		/etc/krb5.keytab
+if [ -d "$sourceRootDir/etc" ]; then
 
-	cat $rootDir/etc/hosts > /etc/hosts
-	cat $rootDir/etc/hostname > /etc/hostname
-	cat $rootDir/etc/hostname > /etc/hostname.ipa-client
-	rm -f $rootDir/etc/hostname
-	rm -f $rootDir/etc/hosts
+	/bin/mv -f $sourceRootDir/etc/sssd/sssd.conf 		/etc/sssd/sssd.conf
+	/bin/mv -f $sourceRootDir/etc/sysconfig/ntpd		/etc/sysconfig/
+	/bin/mv -f $sourceRootDir/etc/ntp/step-tickers	/etc/ntp/step-tickers
+	/bin/mv -f $sourceRootDir/etc/krb5.conf			/etc/krb5.conf
+	/bin/mv -f $sourceRootDir/etc/ipa/ca.crt 			/etc/ipa/ca.crt
+	/bin/mv -f $sourceRootDir/etc/ipa/default.conf 	/etc/ipa/default.conf
+	/bin/mv -f $sourceRootDir/etc/openldap/ldap.conf 	/etc/openldap/ldap.conf
+	/bin/mv -f $sourceRootDir/etc/pki/nssdb/* 		/etc/pki/nssdb
+	/bin/mv -f $sourceRootDir/etc/krb5.keytab 		/etc/krb5.keytab
+
+	cat $sourceRootDir/etc/hosts > /etc/hosts
+	cat $sourceRootDir/etc/hostname > /etc/hostname
+	cat $sourceRootDir/etc/hostname > /etc/hostname.ipa-client
+	rm -f $sourceRootDir/etc/hostname
+	rm -f $sourceRootDir/etc/hosts
 fi
