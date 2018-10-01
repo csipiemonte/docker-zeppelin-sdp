ea22775daf89714dea565775b58534d42571e341
Pietro Cannalire <pierocann93@hotmail.it>
2018-10-01 17:07:17 +0200

    Fix manage notebooks

--- manage-notebooks.sh
+++ manage-notebooks.sh
@@ -10,6 +10,10 @@ case "$1" in
 	"import" )
 		sourceNotebookDir="/var/lib/zeppelin/notebook-backup"	# Kubernetes volume mount (only .json notebook files)
 		destNotebookDir="/var/lib/zeppelin/notebook"
+		
+		if [ ! -d $destNotebookDir ] ; then
+			mkdir -p $destNotebookDir
+		fi
 
 		if [ -z "$(jq --version)" ] ; then
 			wget -q https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O jq && \
@@ -17,17 +21,24 @@ case "$1" in
 			mv jq /usr/local/bin
 		fi
 		
-		FILES=$sourceNotebookDir/*.json
-		for file in $FILES
+		# Create directory structure for Zeppelin notebook import
+		ITEMS=$sourceNotebookDir/*
+		for item in $ITEMS
 		do
-			echo "file: $file"
-			if [ -n "$file" ] ; then
-					id=$(jq '.id' $file | tr -d \")
-					echo "id: $id"
-					mkdir $destNotebookDir/$id
-					cp $file $destNotebookDir/$id/note.json
+			echo "item: $item"
+			if [ -n "$item" -a -d $item ] ; then
+				echo "find directory $item to import to $destNotebookDir"
+			fi
+
+			if [ -n "$item" -a -f $item ] ; then
+				id=$(jq '.id' $item | tr -d \")
+				echo "id: $id"
+				mkdir $sourceNotebookDir/$id
+				mv $item $sourceNotebookDir/$id/$id.json
 			fi
 		done
+
+		cp -rf -d $sourceNotebookDir/* $destNotebookDir
 	;;
 	
 	"backup" )
@@ -36,7 +47,7 @@ case "$1" in
 		if [ -n "$2" ] ; then
 			destRootDir="$2"
 		else
-			destRootDir="/var/lib/zeppelin/notebook-backup/notebook"
+			destRootDir="/var/lib/zeppelin/notebook-backup"
 		fi
 
 		sourceRootDir="/var/lib/zeppelin/notebook"
@@ -47,18 +58,18 @@ case "$1" in
 			rm -rf $destRootDir/*
 		fi
 
-		DIRS=$sourceRootDir/*
-		for dir in $DIRS
-		do
-			if [ -n "$dir" ] ; then
-				echo "dir: $dir / $(basename $dir)"
-				mkdir $destRootDir/$(basename $dir)
-				echo "1: $dir/*.json"				
-				cp $dir/*.json $destRootDir/$(basename $dir)/$(basename $dir).json
-			fi
-		done
+		#DIRS=$sourceRootDir/*
+		#for dir in $DIRS
+		#do
+		#	if [ -n "$dir" ] ; then
+		#		echo "dir: $dir / $(basename $dir)"
+		#		mkdir $destRootDir/$(basename $dir)
+		#		echo "1: $dir/*.json"				
+		#		cp $dir/*.json $destRootDir/$(basename $dir)/$(basename $dir).json
+		#	fi
+		#done
 		
-		#cp -r -d $sourceRootDir/* $destRootDir
+		cp -r -d $sourceRootDir/* $destRootDir
 	;;
 	
 	"restore" )
@@ -67,7 +78,7 @@ case "$1" in
 		if [ -n "$2" ] ; then
 			sourceRootDir="$2"
 		else
-			sourceRootDir="/var/lib/zeppelin/notebook-backup/notebook"
+			sourceRootDir="/var/lib/zeppelin/notebook-backup"
 		fi
 
 		destRootDir="/var/lib/zeppelin/notebook"
@@ -75,7 +86,7 @@ case "$1" in
 		if [ ! -d $sourceRootDir ] ; then
 			echo "$sourceRootDir does not exist"
 			echo "Notebooks not restored"
-		else		
+		else
 			cp -r $sourceRootDir/* $destRootDir
 		fi
 
