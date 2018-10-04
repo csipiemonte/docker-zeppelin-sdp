5736e41f05c571b85ccf15beff9286c4fc30d3f3
Pietro Cannalire <pierocann93@hotmail.it>
2018-10-04 12:47:20 +0200

    Remove installation of jq for manage-notebook.sh. Fix installation of ipa client only on the first execution of the container (the host is new). Use backup files for next runs.

--- manage-notebooks.sh
+++ manage-notebooks.sh
@@ -8,17 +8,12 @@ fi
 case "$1" in
 
 	"import" )
+	
 		sourceNotebookDir="/var/lib/zeppelin/notebook-backup"	# Kubernetes volume mount (only .json notebook files)
 		destNotebookDir="/var/lib/zeppelin/notebook"
 		
 		if [ ! -d $destNotebookDir ] ; then
 			mkdir -p $destNotebookDir
-		fi
-
-		if [ -z "$(jq --version)" ] ; then
-			wget -q https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -O jq && \
-			chmod +x jq && \
-			mv jq /usr/local/bin
 		fi
 		
 		# Create directory structure for Zeppelin notebook import
@@ -32,7 +27,7 @@ case "$1" in
 
 			if [ -n "$item" -a -f $item ] ; then
 				id=$(jq '.id' $item | tr -d \")
-				echo "id: $id"
+				echo "find file json $item to import to $destNotebookDir"
 				mkdir $sourceNotebookDir/$id
 				mv $item $sourceNotebookDir/$id/note.json
 			fi
@@ -55,20 +50,7 @@ case "$1" in
 
 		if [ ! -d $destRootDir ] ; then
 			mkdir -p $destRootDir
-		else
-			rm -rf $destRootDir/*
 		fi
-
-		#DIRS=$sourceRootDir/*
-		#for dir in $DIRS
-		#do
-		#	if [ -n "$dir" ] ; then
-		#		echo "dir: $dir / $(basename $dir)"
-		#		mkdir $destRootDir/$(basename $dir)
-		#		echo "1: $dir/*.json"				
-		#		cp $dir/*.json $destRootDir/$(basename $dir)/$(basename $dir).json
-		#	fi
-		#done
 		
 		cp -r -d $sourceRootDir/* $destRootDir
 		echo "Backup completed"
